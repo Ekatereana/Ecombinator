4794388409efef75d97c3922cd8a420d7680c61c
Kateryna Hrytsaienko <kateryna.hrytsaienko@valtech.com>
2022-05-08 19:40:36 +0300

    add infrastructure

---
+++ main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      # Root module should specify the maximum provider version
+      # The ~> operator is a convenient shorthand for allowing only patch releases within a specific minor release.
+      version = "~> 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# create the resource group
+
+resource "azurerm_resource_group" "resource_group" {
+  name = "${var.project}-${var.environment}-resource-group"
+  location = var.location
+}
+
+# create storage account 
+
+resource "azurerm_storage_account" "storage_account" {
+  name = "${var.project}${var.environment}storage"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = var.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+# create app insigths
+
+resource "azurerm_application_insights" "application_insights" {
+  name                = "${var.project}-${var.environment}-application-insights"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  application_type    = "Node.JS"
+}
+
+# create app service plan
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "${var.project}-${var.environment}-app-service-plan"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.location
+  kind                = "FunctionApp"
+  reserved = true # this has to be set to true for Linux. Not related to the Premium Plan
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+# create app insigths 
+
+resource "azurerm_function_app" "function_app" {
+  name                       = "${var.project}-${var.environment}-function-app"
+  resource_group_name        = azurerm_resource_group.resource_group.name
+  location                   = var.location
+  app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "",
+    "FUNCTIONS_WORKER_RUNTIME" = "node",
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key,
+  }
+  os_type = "linux"
+  site_config {
+    linux_fx_version          = "node|14"
+    use_32_bit_worker_process = false
+  }
+  storage_account_name       = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+  version                    = "~3"
+
+  lifecycle {
+    ignore_changes = [
+      app_settings["WEBSITE_RUN_FROM_PACKAGE"],
+    ]
+  }
+}
